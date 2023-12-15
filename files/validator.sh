eb864dfd5099fd5eaf256e40f19a469ff580ad78
grinchify <83351519+grinchify@users.noreply.github.com>
2023-12-15 17:46:45 -0500

    Add files via upload

---
+++ files/validator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+export SOLANA_METRICS_CONFIG="host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password"
+exec /mnt/solana/target/release/solana-validator \
+--identity /home/solana/rpc_node.json \
+--entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
+--rpc-port 8899 \
+--dynamic-port-range 8002-8099 \
+--no-port-check \
+--gossip-port 8001 \
+--no-untrusted-rpc \
+--no-voting \
+--private-rpc \
+--rpc-bind-address 0.0.0.0 \
+--enable-cpi-and-log-storage \
+--account-index program-id \
+--enable-rpc-transaction-history \
+--no-duplicate-instance-check \
+--wal-recovery-mode skip_any_corrupted_record \
+--log /mnt/logs/solana-validator.log \
+--accounts /mnt/solana-accounts \
+--ledger /mnt/solana-ledger \
+--snapshots /mnt/solana-snapshots \
+--no-snapshot-fetch \
+--limit-ledger-size 400000000 \
+--rpc-send-default-max-retries 3 \
+--rpc-send-service-max-retries 3 \
+--rpc-send-retry-ms 2000 \
+--full-rpc-api \
+--accounts-index-memory-limit-mb 350 \
+--account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
+--account-index-exclude-key metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s \
+--tpu-use-quic \
+--known-validator DRpbCBMxVnDK7maPM5tGv6MvB3v1sRMC86PZ8okm21hy \
+--known-validator EgxVyTgh2Msg781wt9EsqYx4fW8wSvfFAHGLaJQjghiL \
+--known-validator PUmpKiNnSVAZ3w4KaFX6jKSjXUNHFShGkXbERo54xjb \
+--known-validator AHCGTSrztg5PqxDN1pi59YQbJE4wMHyBrsZ9P6DrTui3 \
+--known-validator 4VrjyXQT61WFSjuG3ehgqZUK1jqvYqB46veQbXLotq3n \
+--known-validator AnoQ3whjumVaJsBX4r7skgGebzq2sk2XTqPtV7tqnTHf \
+--known-validator 8MuGDCx6q7fEz52yoHoiDby4TEVJncYuWCGAjCWSrtPx
